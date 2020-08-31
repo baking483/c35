bdcec20b081b1fbec1fb01240de590d9e613d117
baking483 <64713701+baking483@users.noreply.github.com>
2020-08-30 19:50:18 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState = 0;
+var playerCount,database;
+var form,player,game;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(400,400);
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    
+}
+
